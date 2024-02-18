66e426b482c45444f291a588163130e8188d4c03
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-19 00:15:40 +0530

    search functionality added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,10 @@ import WeatherDetails from "@/components/WeatherDetails";
 import { metersToKilometers } from "@/utils/metersToKilometers";
 import { convertWindSpeed } from "@/utils/convertWindSpeed";
 import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
+import { loadingCityAtom, placeAtom } from "./atom";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+
 
 interface WeatherDetail {
   dt: number;
@@ -71,25 +75,47 @@ interface WeatherData {
 
 
 export default function Home() {
-  const { isLoading, error, data} = useQuery<WeatherData>("repoData", async () => {
+
+  const [place, setPlace] = useAtom(placeAtom);
+  const [loadingCity] = useAtom(loadingCityAtom);
+
+
+  const { isLoading, error, data , refetch} = useQuery<WeatherData>("repoData", async () => {
     const {data} = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
     );
     return data;
   }
   );
+  
+  useEffect(() => {
+    refetch();
+  }, [place, refetch]);
+
 
   const firstData = data?.list[0];
 
   console.log("data", data);
 
-  const uniqueDates = [
-    ...new Set(
-      data?.list.map(
-        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
-      )
-    )
-  ]
+  const today = new Date();
+// Array to store the next 5 dates
+const nextFiveDates = [];
+
+// Loop to get the next 5 dates after today
+for (let i = 0; i < 5; i++) {
+  const nextDate = new Date(today);
+  nextDate.setDate(today.getDate() + i);
+  nextFiveDates.push(nextDate.toISOString().split("T")[0]);
+}
+
+   // Filtering data to get the first entry after 6 AM for each unique date
+   const firstDataForEachDate = nextFiveDates.map(date => {
+    return data?.list.find(entry => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      const entryTime = new Date(entry.dt * 1000).getHours();
+      return entryDate === date && entryTime >= 6;
+    });
+  });
   
   if(isLoading)  return (
     <div className="flex items-center min-h-screen justify-center">
@@ -197,7 +223,31 @@ export default function Home() {
       {/* 5 days forecast data */}
       <section className="flex w-full flex-col gap-4  ">
         <p className="text-2xl">ForeCast (5 Days)</p>
-          <ForecastWeatherDetail />
+        {firstDataForEachDate.map((d, i) => (
+          <ForecastWeatherDetail
+            key={i}
+            description={d?.weather[0].description ?? ""}
+            weatehrIcon={d?.weather[0].icon ?? "01d"}
+            date={format(parseISO(d?.dt_txt ?? ""), "dd.MM")}
+            day={format(parseISO(d?.dt_txt ?? ""), "EEEE")}
+            feels_like={d?.main.feels_like ?? 0}
+            temp={d?.main.temp ?? 0}
+            temp_max={d?.main.temp_max ?? 0}
+            temp_min={d?.main.temp_min ?? 0}
+            airPressure={`${d?.main.pressure} hPa `}
+            humidity={`${d?.main.humidity}% `}
+            sunrise={format(
+              fromUnixTime(data?.city.sunrise ?? 1702517657),
+              "H:mm"
+            )}
+            sunset={format(
+              fromUnixTime(data?.city.sunset ?? 1702517657),
+              "H:mm"
+            )}
+            visability={`${metersToKilometers(d?.visibility ?? 10000)} `}
+            windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
+          />
+        ))}
       </section>
      </main>
    </div>
